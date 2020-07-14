59a80c80136292da353d1270d9b76e18ce81b8df
Daniel <dan-santiago@live.com>
2020-07-14 15:47:38 -0400

    Rotas e Autenticação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,12 +12,13 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction)=>{
-  if(err instanceof AppError){
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
@@ -28,7 +30,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction)=>{
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
-  })
+  });
 });
 
 app.listen(3333, () => console.log('Server started on port 3333!'));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import GlobaStyle from './styles/global';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+
+import AppProvider from './hooks';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
-      <SignIn />
+    <Router>
+      <AppProvider>
+          <Routes />
+      </AppProvider>
+
       <GlobaStyle />
-    </>
+    </Router>
   );
 }
 
